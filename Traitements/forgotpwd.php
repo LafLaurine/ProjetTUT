90ea77260fdb736a51b9220ed3ccaf0bade37138
Laurine <30570842+LafLaurine@users.noreply.github.com>
2017-10-28 22:22:06 -0400

    Add files via upload

--- Traitements/forgotpwd.php
+++ Traitements/forgotpwd.php
@@ -15,8 +15,8 @@
 <body>
 
 
-<form action="change.php" method="POST">
-E-mail Address: <input type="text" name="email" size="20" /> <input type="submit" name="ForgotPassword" value=" Request Reset " />
+<form action="forgotPassword.php" method="POST">
+E-mail Address: <input type="text" name="email" size="20" /> <input type="submit" name="ForgotPassword" value="Valider" />
 </form>
 
 </body>
