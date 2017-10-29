87d3af8447c9b1d3b71ab4c6ea15dbc8fec39ca2
Laurine <30570842+LafLaurine@users.noreply.github.com>
2017-10-29 19:26:21 -0400

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,15 +1,32 @@
-$(document).ready(function()
-{
-  $("#info").click(function(){
-    var $target = $('.information'),
-        $info = $(this);
+$(document).ready(function () {
 
-    $target.slideToggle(200, function () {
-        $info.text(($target.is(':visible') ? 'Informations' : 'Informations'));
+
+    $("#info").click(function () {
+        var $target = $('.information'),
+            $info = $(this);
+
+        $target.slideToggle(200, function () {
+            $info.text(($target.is(':visible') ? 'Informations' : 'Informations'));
+            $("#video").css({ "height": "30.5em" });
+        });
+
     });
 
- 
+    $("body").css("display", "none");
+    
+        $("body").fadeIn(1000);
+        
+        $("a.transition").click(function(event){
+            event.preventDefault();
+            linkLocation = this.href;
+            $("body").fadeOut(500, redirectPage);		
+        });
+            
+        function redirectPage() {
+            window.location = linkLocation;
+        }
 
     
-});
+
+
 });
